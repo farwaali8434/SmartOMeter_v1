3295951c7fed384a4661bf37bf76b2d40de3572c
Wadood <wadoodislam@gmail.com>
2018-12-04 01:44:52 +0500

    [WIP] Auth Working

--- SmartOMeter_v1/settings.py
+++ SmartOMeter_v1/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
--- SmartOMeter_v1/urls.py
+++ SmartOMeter_v1/urls.py
@@ -22,6 +22,8 @@ from userportal import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'citys', views.CityViewSet)
+router.register(r'areas', views.AreaViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
