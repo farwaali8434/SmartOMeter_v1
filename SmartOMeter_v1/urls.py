4557834670f98aab6e0ef1c2040f8224821c4b0b
farwaali8434 <41327806+farwaali8434@users.noreply.github.com>
2018-12-04 02:49:42 +0500

    registered model with admin

--- SmartOMeter_v1/urls.py
+++ SmartOMeter_v1/urls.py
@@ -17,17 +17,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
-from SmartOMeter_v1.userportal import views
+from userportal import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^', include(router.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- userportal/admin.py
+++ userportal/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from userportal.models import *
+
+admin.site.register(Ticket)
+admin.site.register(Message)
+admin.site.register(City)
+admin.site.register(Area)
+admin.site.register(Meter)
+admin.site.register(Invoice)
+admin.site.register(Subscription)
+admin.site.register(Announcement)
+admin.site.register(Consumption)
+admin.site.register(Profile)
