a1db50a2622f5ef8d5cc8015e2da73100f920683
Wadood <wadoodislam@gmail.com>
2019-01-07 15:17:17 +0500

    Token Authentication and Ticket Api 1/2.

--- userportal/views.py
+++ userportal/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, generics
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from userportal.models import City, Area, Ticket, Invoice, Message
 from userportal.models import Consumption, Announcement, Profile, Subscription, Meter
