cf687f99f0aaf2100c215e074220bd6984df7f25
farwaali <farwaali.me@gmail.com>
2019-03-23 22:52:47 +0500

    single cunsumer and msg echo

--- SmartOMeter_v1/settings.py
+++ SmartOMeter_v1/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SmartOMeter_v1.wsgi.application'
 ASGI_APPLICATION = 'SmartOMeter_v1.routing.application'
+CHANNEL_LAYERS = {
+    # 'default': {
+    #     'BACKEND': 'channels_redis.core.RedisChannelLayer',
+    #     'CONFIG': {
+    #         "hosts": [('127.0.0.1', 6379)],
+    #     },
+    # },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
