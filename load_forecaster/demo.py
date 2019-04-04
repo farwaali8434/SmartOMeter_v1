3ac1372bf8fea0aa3d19f38b7a95eb331c939fda
farwaali <farwaali.me@gmail.com>
2019-04-04 05:51:36 +0500

    demo uncomment

--- load_forecaster/demo.py
+++ load_forecaster/demo.py
@@ -3,26 +3,26 @@ import load_forecaster.sm_forcaster as sm
 from load_forecaster.LoadForecaster import Forecaster
 
 datafile = 'data/NCENT.csv'
-useful_data = 'data/useful_data.csv'
+# useful_data = 'data/useful_data.csv'
 df = pd.read_csv(datafile)
 
 all_x = sm.make_useful_df(df)
 # all_x.to_csv('data/useful_data.csv')
 # all_x = pd.read_csv(useful_data)
-# all_y = df['load']
-#
-# train_x, train_y = all_x[:-8760], all_y[:-8760]
-# test_x, test_y = all_x[-8760:], all_y[-8760:]
-#
-# forecaster = sm.neural_net_model(train_x, train_y)
-# train_predictions = [float(f) for f in forecaster.predict(train_x)]
-# test_predictions = [float(f) for f in forecaster.predict(test_x)]
-#
-# train_accuracy = sm.MAPE(train_predictions, train_y)
-# test_accuracy = sm.MAPE(test_predictions, test_y)
-#
-# print('Percent accuracy (MAPE). Train: {}.  Test: {}'.format(100-train_accuracy, 100-test_accuracy))
-#
+all_y = df['load']
+
+train_x, train_y = all_x[:-8760], all_y[:-8760]
+test_x, test_y = all_x[-8760:], all_y[-8760:]
+
+forecaster = sm.neural_net_model(train_x, train_y)
+train_predictions = [float(f) for f in forecaster.predict(train_x)]
+test_predictions = [float(f) for f in forecaster.predict(test_x)]
+
+train_accuracy = sm.MAPE(train_predictions, train_y)
+test_accuracy = sm.MAPE(test_predictions, test_y)
+
+print('Percent accuracy (MAPE). Train: {}.  Test: {}'.format(100-train_accuracy, 100-test_accuracy))
+
 # forecaster.save('checkpoint/forecaster.h5')
 # del forecaster
 # forecaster_reloaded = Forecaster('checkpoint/forecaster.h5')
