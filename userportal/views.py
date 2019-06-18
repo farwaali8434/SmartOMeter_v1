abb90badfea5c08873f3f48472ab1da729765078
Wadood <wadoodislam@gmail.com>
2019-06-18 06:57:04 +0500

    Chat in smartometer. (Complete)

--- userportal/views.py
+++ userportal/views.py
@@ -254,3 +254,7 @@ class TicketListView(LoginRequiredMixin, ListView):
 
 class TicketDetailView(LoginRequiredMixin, DetailView):
     model = Ticket
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['user'] = self.request.user
